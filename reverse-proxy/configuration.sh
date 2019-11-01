2f6b397853c96cdf95501c299075e1d51b89c179
Eti <nehu@Pixel>
2019-11-01 12:53:17 +0100

    Generate the nginx configuration using environment variables

---
+++ reverse-proxy/configuration.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+set -e
+
+export HTPASSWD="/srv/.htpasswd"
+
+export _indentationLevel=0
+
+generateHTPasswd()
+{
+	local i
+	for ((i = 0; i < 32767; ++i)); do
+		userEntry="$(eval 'echo -n $user'$i)"
+
+		if [ -n "$userEntry" ]; then
+			echo "$userEntry"
+		else
+			break;
+		fi
+	done
+}
+
+generateConfiguration()
+{
+	_writeEvents
+	_writeHttp
+}
+
+_openBlock()
+{
+	if [ "$#" -ne 1 ]; then
+		echo "_openBlock() requires the block name as an argument" >> /dev/stderr
+		exit 1
+	fi
+
+	_writeIndent
+	echo "$1 {"
+
+	export _indentationLevel=$(($_indentationLevel + 1))
+}
+
+_closeBlock()
+{
+	if [ "$_indentationLevel" -gt 0 ]; then
+		export _indentationLevel=$(($_indentationLevel - 1))
+	fi
+
+	_writeIndent
+	echo "}"
+}
+
+_writeIndent()
+{
+	local i
+	for ((i = 0; i < "$_indentationLevel"; ++i)); do
+		echo -n -e "\t"
+	done
+}
+
+_writeEntry()
+{
+	if [ "$#" -lt 2 ]; then
+		echo "_writeEntry() requires at least two parameters" >> /dev/stderr
+		exit 1
+	fi
+
+	_writeIndent
+
+	echo -n "$1"
+
+	local i
+	for ((i = 2; i <= "$#"; ++i)); do
+		echo -n " $(eval 'echo -n $'$i)"
+	done
+
+	echo ";"
+}
+
+_writeEvents()
+{
+	_openBlock "events"
+	_closeBlock
+}
+
+_writeHttp()
+{
+	_openBlock "http"
+
+	_writeEntry "log_format" "main" \
+		'$remote_addr - $remote_user [$time_local] "$request" ' \
+		'$status $body_bytes_sent "$http_referer" ' \
+		'"$http_user_agent" "$http_x_forwarded_for"';
+
+	_writeServer
+
+	_closeBlock
+}
+
+_writeServer()
+{
+	_openBlock "server"
+
+	_writeEntry "listen" "80"
+	_writeEntry "server_name" "$server_name"
+
+	_writeLocations
+
+	_closeBlock
+}
+
+_writeLocations()
+{
+	local i
+	for ((i = 0; i < 32767; ++i)); do
+		location_source="$(eval 'echo -n $location'$i'_source')"
+		location_destination="$(eval 'echo -n $location'$i'_destination')"
+		location_basicauth="$(eval 'echo -n $location'$i'_basicauth')"
+
+		if [ -n "$location_source" ]; then
+			if [ -z "$location_basicauth" ]; then
+				location_basicauth=0
+			fi
+
+			_writeLocation "$location_source" "$location_destination" "$location_basicauth"
+		else
+			break;
+		fi
+	done
+}
+
+_writeLocation()
+{
+	if [ "$#" -ne 3 ]; then
+		echo "_writeLocation() requires three parameters" >> /dev/stderr
+		exit 1
+	fi
+
+	location_source=$1
+	location_destination=$2
+	location_basicauth=$3
+
+	_openBlock "location $location_source"
+
+	if [ "$location_basicauth" -gt 0 ]; then
+		_writeEntry "auth_basic" "$server_name"
+		_writeEntry "auth_basic_user_file" "$HTPASSWD"
+	fi
+
+	_writeEntry "proxy_pass" "$location_destination"
+	_writeEntry "proxy_set_header" "Upgrade" '$http_upgrade';
+	_writeEntry "proxy_set_header" "Connection" '"Upgrade"';
+	_writeEntry "proxy_set_header" "Accept-Encoding" "gzip";
+
+	_closeBlock
+}
