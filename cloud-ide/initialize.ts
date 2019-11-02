e810405dc3d38acb7f4995f8f2350a63ce033e38
Eti <nehu@Pixel>
2019-11-02 15:29:38 +0100

    Use a forked version of shelljs to allow using chown() in the initialization script

--- cloud-ide/initialize.ts
+++ cloud-ide/initialize.ts
@@ -8,11 +8,13 @@ sh.cd("/opt");
 sh.echo("Installing docker-cli");
 sh.exec("curl -o docker-cli.apk http://dl-cdn.alpinelinux.org/alpine/latest-stable/community/x86_64/docker-cli-18.09.8-r0.apk").toEnd("/dev/null");
 sh.exec("apk add --no-cache docker-cli.apk").toEnd("/dev/null");
+sh.exec("groupadd docker");
+sh.exec("usermod -a -G docker theia");
 
 // SSH convenience
 let knownHost : string = "";
 sh.mkdir("-p", "/home/theia/.ssh");
-sh.exec("chown theia:theia /home/theia/.ssh");
+sh.chown("-R", "theia:theia", "/home/theia/.ssh");
 sh.chmod(700, "/home/theia/.ssh");
 sh.echo(knownHost).toEnd("/home/theia/.ssh/known_hosts");
 
